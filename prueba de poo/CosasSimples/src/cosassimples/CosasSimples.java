f89861f76a41b89c1e61ad2ba14b3be31c8c67cc
Fabrizzio <fabzzc31@gmail.com>
2025-04-27 18:20:38 -0500

    viendo si se eliminaros las 2 cosas que agregue

--- prueba de poo/CosasSimples/src/cosassimples/CosasSimples.java
+++ prueba de poo/CosasSimples/src/cosassimples/CosasSimples.java
@@ -16,8 +16,6 @@ public class CosasSimples {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola, me llamo Fabrizzio");
-        System.out.println("Probando el segundo commit");
-        System.out.println("Ejemplo12345678910");
     }
     
 }
